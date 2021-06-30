7abe2a61d2949570e67bfc9c00d33ab412ad0cea
GhoshDebashree <shree19ghosh@gmail.com>
2021-06-30 17:33:04 +0530

    Check smallest element in an array

--- Arrays/TestPrimeNumbers/Arrays/CheckMinimumTwo.java
+++ Arrays/TestPrimeNumbers/Arrays/CheckMinimumTwo.java
@@ -14,5 +14,5 @@ public static void main(String[] args){
   int[] arr = {10,20,30,2,7,5,1,3};
   int resultSmallest = findMinimum(arr);
   System.out.println("Smallest in the array is : " +resultSmallest);
-}
+  }
 }
