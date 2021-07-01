56c5d3f8f47d733b8a35b185d4929936a9734a2e
GhoshDebashree <shree19ghosh@gmail.com>
2021-07-01 20:38:43 +0530

    Rearrange the negative and positive elements in an array

---
+++ Arrays/TestPrimeNumbers/Arrays/CheckRearrange.java
@@ -0,0 +1,26 @@
+package Arrays.TestPrimeNumbers.Arrays;
+
+class CheckReArrange {
+
+  public static void reArrange(int[] arr) {
+    int j = 0; 
+    for (int i = 0; i < arr.length; i++) { 
+      if (arr[i] < 0) {   // if negative number found
+          int temp = arr[i];
+          arr[i] = arr[j]; // swapping with leftmost positive
+          arr[j] = temp;
+       
+        j++; 
+      } 
+    } 
+  }
+  public static void main(String[] args){
+    int[] arr = {1,-1,3,-2,-4,3,5,2,-4,-2};
+    
+    reArrange(arr);
+    for(int i = 0; i< arr.length; i++){
+      System.out.println(arr[i]);
+    
+}
+}
+}
