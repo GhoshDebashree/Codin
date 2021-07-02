c4b7b15db43418e02e8b4907fdf2b2b69d02a59e
GhoshDebashree <shree19ghosh@gmail.com>
2021-07-02 17:23:20 +0530

    Remove element from an array

---
+++ Arrays/TestPrimeNumbers/Arrays/RemoveElement.java
@@ -0,0 +1,25 @@
+package Arrays.TestPrimeNumbers.Arrays;
+
+class RemoveElement {
+  public static int removeElement(int[] nums, int val) {
+      int i = 0;
+      for(int j = 0;j < nums.length; j++){
+          if(nums[j] == val){
+              continue;
+          }
+          else {    
+              nums[i] = nums[j];
+                 i++;
+          }
+        }
+      
+  return i ; 
+      
+}
+  public static void main(String[] args){
+    int[] nums = {1,2,3,4,5};
+    int val = 3;
+    int res = removeElement(nums,val);
+    System.out.println(res);
+   }
+}
